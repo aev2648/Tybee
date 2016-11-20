db42ef98bf047c2663266203491b6594cc4b5f02
Alances Vargas (RIT Student) <aev2648@ad.rit.edu>
2016-11-19 21:38:47 -0500

    The second Commit, basic set up

---
+++ Tybee/Assets/Scripts/BeeScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeScript : MonoBehaviour {
+    public string word { get; set; }
+    public float speed = 0;
+
+	// Use this for initialization
+	void Awake () {
+        word = "default";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+}
---
+++ Tybee/Assets/Scripts/Manager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject BeePF;
+
+	// Use this for initialization
+	void Start () {
+        GameObject Bee = GameObject.Instantiate(BeePF);
+        Text Bword = GameObject.Find("Bee").GetComponent<Text>() as Text;
+        Bword.text = Bee.GetComponent<BeeScript>().word;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
